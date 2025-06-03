7fc85aa9a9bf4b8409dcccfe1db7a9608b55da69
tail-s <lunar7s@naver.com>
2025-06-03 19:40:56 +0900

    INITIAL COMMIT

---
+++ anything.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	foo()
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
