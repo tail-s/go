768bb3b465b002e1182eced6a7f8e9234c10e13c
tail-s <lunar7s@naver.com>
2025-06-19 00:20:38 +0900

    250619

--- identifier.go
+++ identifier.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var hello = 12
+
 func main() {
 	a := 1
 	fmt.Println(a)
@@ -11,4 +13,12 @@ func main() {
 	fmt.Println(x)
 	y := 100 + 55
 	fmt.Println(y)
+
+	var hi = 43
+	fmt.Println(hi)
+	test()
+}
+
+func test() {
+	fmt.Println("This is var", hello)
 }
